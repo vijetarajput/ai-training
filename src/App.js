f5fe9c398b129121120dd6d192d12582953c7692
Vijeta Singh Rajput <vijetasinghrajput@gmail.com>
2023-12-11 14:24:13 +0000

    api called

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(function (response) {
-    console.log('Data:', response.data);
-  })
-  .catch(function (error) {
-    console.error('Error:', error);
-  });
+  
   return (
     <div className="App">
       
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+// reportWebVitals();
+
+let apiKey = "03ob4taaa6f366304d608c02ae4ad453";
+let prompt = "Be polite and provide a short answer";
+let context ="When was the first AI created?";
+let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+console.log("Processing...");
+axios.get(apiUrl).then(showResult);
+
+function showResult(response)
+{
+  console.log(response.data.answer);
+  return(
+  <h1>This is axios</h1>
+);}
